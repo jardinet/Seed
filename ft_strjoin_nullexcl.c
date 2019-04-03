9db9e9758c785a3a19ad5f94eb56472f6583c52b
Marie WRAGG <mwragg@student.42.fr>
2019-04-03 21:44:11 +0200

    strjoin revision

--- ft_strjoin_nullexcl.c
+++ ft_strjoin_nullexcl.c
@@ -6,7 +6,7 @@
 /*   By: mwragg <mwragg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 17:37:08 by mwragg            #+#    #+#             */
-/*   Updated: 2019/04/03 21:41:23 by mwragg           ###   ########.fr       */
+/*   Updated: 2016/12/01 14:39:20 by mwragg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char *link;
 
-	if (!s1 && !s2)
+	if ((!s1 || !s2) || (link = (char *)malloc(sizeof(char) *
+		(ft_strlen(s1) + ft_strlen(s2) + 1))) == NULL)
 		return (NULL);
-	else if (!s1)
-		return (ft_strdup(s2));
-	else if (!s2)
-		return (ft_strdup(s1));
-	else
-	{
-		if ((link = (char *)malloc(sizeof(char) *
-						(ft_strlen(s1) + ft_strlen(s2) + 1))) == NULL)
-			return (NULL);
-		return (ft_strcat(ft_strcpy(link, s1), s2));
-	}
+	return (ft_strcat(ft_strcpy(link, s1), s2));
 }
