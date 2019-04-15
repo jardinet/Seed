1e9f352ba1acc08a8f94679182729482f0d261d3
Marie WRAGG <mwragg@student.42.fr>
2019-04-15 13:55:36 +0200

    mh

--- ft_strnjoin_free.c
+++ ft_strnjoin_free.c
@@ -6,7 +6,7 @@
 /*   By: mwragg <mwragg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 18:43:33 by mwragg            #+#    #+#             */
-/*   Updated: 2019/04/09 21:10:03 by mwragg           ###   ########.fr       */
+/*   Updated: 2019/04/11 22:00:42 by mwragg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_strnjoin_free(char const *s1, char const *s2, size_t len, int f)
 	if (!s1 && !s2)
 		return (NULL);
 	else if (!s1)
-		link = ft_strsub(s2, s2, len);
+		link = ft_strsub(s2, 0, len);
 	else if (!s2)
-		link = ft_strsub(s1, s1, len);
+		link = ft_strsub(s1, 0, len);
 	else
 	{
 		if ((link = (char *)malloc(sizeof(char) *
