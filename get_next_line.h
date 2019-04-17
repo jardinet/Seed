a2d140a024dea1b743a75f8b9eab6d7a5f0559a3
Marie WRAGG <mwragg@student.42.fr>
2019-04-17 20:26:55 +0200

    VIIIIIICTOOOOOOR

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwragg <mwragg@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/31 10:19:09 by mwragg            #+#    #+#             */
+/*   Updated: 2019/04/17 14:17:29 by mwragg           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "./libft/includes/libft.h"
+# define BUFF_SIZE 25
+# define CUT_CHAR '\n'
+
+typedef struct	s_fd
+{
+	int			fd;
+	int			ret;
+	char		buf[BUFF_SIZE + 1];
+	int			len;
+	int			start;
+}				t_fd;
+
+int				get_next_line(const int fd, char **line);
+
+#endif
